3a85413c1773335e15dfa8e933981caec9e6b340
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-09-06 17:21:38 -0500

    Summary broken into 2 components

--- src/SummarySection/SummarySection.js
+++ src/SummarySection/SummarySection.js
@@ -1,42 +1,15 @@
 import React from 'react';
-
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
+import SummaryCart from '../SummaryCart/SummaryCart';
+import SummaryTotal from '../SummaryTotal/SummaryTotal';
 
 class SummarySection extends React.Component {
   render() {
-    const { selectedFeatures } = this.props
-    const summary = Object.keys(selectedFeatures).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = selectedFeatures[feature];
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-
-    const total = Object.keys(selectedFeatures).reduce(
-      (acc, curr) => acc + selectedFeatures[curr].cost,
-      0
-    );
 
     return (
       <section className="main__summary">
         <h2>Your cart</h2>
-        {summary}
-        <div className="summary__total">
-          <div className="summary__total__label">Total</div>
-          <div className="summary__total__value">
-            {USCurrencyFormat.format(total)}
-          </div>
-        </div>
+          <SummaryCart selectedFeatures={this.props.selectedFeatures} />
+          <SummaryTotal selectedFeatures={this.props.selectedFeatures} />
       </section>
     );
   }
